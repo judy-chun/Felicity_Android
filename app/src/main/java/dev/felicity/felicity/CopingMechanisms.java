0ef2dc39d6f8437bc6997847c54cb48445bc3daa
philliplagoc <philliplagoc@gmail.com>
2018-11-27 19:01:46 -0800

    changed coping_mechanisms screen to accomodate for check boxes

--- app/src/main/java/dev/felicity/felicity/CopingMechanisms.java
+++ app/src/main/java/dev/felicity/felicity/CopingMechanisms.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CopingMechanisms  extends AppCompatActivity {
@@ -17,29 +20,44 @@ public class CopingMechanisms  extends AppCompatActivity {
 
     private ImageButton mNext;
     private HashMap<String,Object> mInfo;
-    private EditText mEdit1;
+//    private EditText mEdit1;
     private EditText mEdit2;
+    private CheckBox[] boxes = new CheckBox[11];
+    private ArrayList<String> copingMechanisms = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coping_mechanisms);
+        boxes[0] = findViewById(R.id.checkBox);
+        boxes[1] = findViewById(R.id.checkBox2);
+        boxes[2] = findViewById(R.id.checkBox3);
+        boxes[3] = findViewById(R.id.checkBox4);
+        boxes[4] = findViewById(R.id.checkBox5);
+        boxes[5] = findViewById(R.id.checkBox6);
+        boxes[6] = findViewById(R.id.checkBox7);
+        boxes[7] = findViewById(R.id.checkBox8);
+        boxes[8] = findViewById(R.id.checkBox9);
+        boxes[9] = findViewById(R.id.checkBox10);
+        boxes[10] = findViewById(R.id.checkBox11);
+
 
         mNext = findViewById(R.id.next);
         mInfo = (HashMap<String,Object>)getIntent().getSerializableExtra("mInfo");
-        mEdit1=findViewById(R.id.editText1);
+//        mEdit1=findViewById(R.id.editText1);
         mEdit2=findViewById(R.id.editText2);
 
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String info1=mEdit1.getText().toString();
+//                String info1=mEdit1.getText().toString();
                 String info2= mEdit2.getText().toString();
-                if(info1.equals("") || info2.equals("")){
+                if(info2.equals("")){
                     Toast.makeText(CopingMechanisms.this,"Fields cannot be empty",Toast.LENGTH_LONG).show();
                 }
                 else {
-                    mInfo.put("CopingMechanisms1", info1);
+//                    mInfo.put("CopingMechanisms1", info1);
+                    mInfo.put("CopingMechanisms", copingMechanisms);
                     mInfo.put("CopingMechanisms2", info2);
                     Intent intentLoadNewActivity = new Intent(CopingMechanisms.this, AvoidanceAssessment.class);
                     intentLoadNewActivity.putExtra("mInfo", mInfo);
@@ -47,5 +65,20 @@ public class CopingMechanisms  extends AppCompatActivity {
                 }
             }
         });
+
+        // Loop through each box and append each checked box to copingMechanisms
+        for(int i=0; i<boxes.length;i++){
+            final CheckBox box = boxes[i];
+            box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        copingMechanisms.add(box.getText().toString());
+                    }
+                    else{
+                        copingMechanisms.remove(box.getText().toString());
+                    }
+                }
+            });
+        }
     }
 }
